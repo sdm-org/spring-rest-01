f2acd02c37ae1fbd17de15b86722b5a56cc81172
Christian Dupuis <cd@atomist.com>
2019-05-02 20:30:03 +0200

    Update SpringRestSeedApplication.java

--- src/main/java/com/atomist/spring/SpringRestSeedApplication.java
+++ src/main/java/com/atomist/spring/SpringRestSeedApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringRestSeedApplication {
 
+	// test
 	public static void main(String[] args) {
 		SpringApplication.run(SpringRestSeedApplication.class, args);
 	}
