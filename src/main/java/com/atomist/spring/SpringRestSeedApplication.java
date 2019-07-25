838f37833304b6331d77d61b2dea8231d28994f1
Christian Dupuis <cd@atomist.com>
2019-07-25 13:15:14 +0200

    Fix tests

--- src/main/java/com/atomist/spring/SpringRestSeedApplication.java
+++ src/main/java/com/atomist/spring/SpringRestSeedApplication.java
@@ -7,8 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringRestSeedApplication {
  
-	// test test test test test test test test test test test test test
-	public static void main(String[] args) {
+	// test test test test test test test test test test test test test test
+	public static void main(String[] args) { 
 		SpringApplication.run(SpringRestSeedApplication.class, args);
 	}
 }
